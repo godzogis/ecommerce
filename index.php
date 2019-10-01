c37dac7d2ddd521aad560861349f8e33745c3569
godzogis <suporte@servicomputer.com.br>
2019-10-01 15:09:55 -0300

    criado pageadmin e TPL admin

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ require_once("vendor/autoload.php");
 
 use \Slim\Slim;
 use \Hcode\Page;
+use \Hcode\PageAdmin;
 
 $app = new Slim();
 
@@ -17,17 +18,18 @@ $app->get('/', function() {
 
 });
 
+
+$app->get('/admin', function() {
+    
+	$page = new PageAdmin();
+
+	$page->setTpl("index");
+
+});
+
+
+
+
 $app->run();
 
- ?>
- <?php
- /*
-
- Primeira aula
-
- 	$sql = new Hcode\DB\Sql();
-	$results = $sql->select("SELECT * FROM tb_users");
-
-	echo json_encode($results);
-*/
  ?>
